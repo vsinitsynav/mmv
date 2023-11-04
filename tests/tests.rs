894a2d8aefac483ea7b549eb58d5a5c25d63be39
vsinitsynav <vladislava.sinitsyna@gmail.com>
2023-11-05 01:22:18 +0300

    fix tests

--- tests/tests.rs
+++ tests/tests.rs
@@ -2,15 +2,17 @@
 
 use mmv::r#move::try_to_move;
 use std::path::Path;
+use tempdir::TempDir;
 
 #[test]
 fn it_works() {
-    let _ = std::fs::File::create("tmp/cbacabc.txt");
-    let _ = std::fs::File::create("tmp/bbacabb.txt");
+    let tmp_dir = TempDir::new("tmp").unwrap();
+    let _ = std::fs::File::create(tmp_dir.path().join("cbacabc.txt"));
+    let _ = std::fs::File::create(tmp_dir.path().join("bbacabb.txt"));
     assert_eq!(
         try_to_move(
-            Path::new("tmp/cbacabc.txt"),
-            Path::new("tmp/a#1a.#3"),
+            &tmp_dir.path().join("cbacabc.txt"),
+            &tmp_dir.path().join("a#1a.#3"),
             "c(.*)(.*)c\\.(.*)",
             false
         ),
@@ -18,8 +20,8 @@ fn it_works() {
     );
     assert_eq!(
         try_to_move(
-            Path::new("tmp/bbacabb.txt"),
-            Path::new("tmp/b#11b.#2"),
+            &tmp_dir.path().join("bbacabb.txt"),
+            &tmp_dir.path().join("b#11b.#2"),
             "a(.*)a\\.(.*)",
             false
         ),
@@ -27,30 +29,31 @@ fn it_works() {
     );
     assert_eq!(
         try_to_move(
-            Path::new("tmp/abacaba.txt"),
-            Path::new("tmp/b#1b.#2"),
+            &tmp_dir.path().join("abacaba.txt"),
+            &tmp_dir.path().join("b#1b.#2"),
             "a(.*)a\\.(.*)",
             false
         ),
-        Err("Not able to replace existing file: tmp/bbacabb.txt".to_string())
+        Err("Not able to replace existing file: ".to_string()
+            + (tmp_dir.path().join("bbacabb.txt").to_str().unwrap()))
     );
     assert_eq!(
         try_to_move(
-            Path::new("tmp/abacaba.txt"),
-            Path::new("tmp/b#1b.#2"),
+            &tmp_dir.path().join("abacaba.txt"),
+            &tmp_dir.path().join("b#1b.#2"),
             "a(.*)a\\.(.*)",
             true
         ),
         Ok(true)
     );
 
-    let _ = std::fs::remove_file("tmp/bbacabb.txt");
-    let _ = std::fs::File::create("tmp/some_A_filename.bin");
-    let _ = std::fs::File::create("tmp/changed_A_filename.bin");
+    let _ = std::fs::remove_file(tmp_dir.path().join("bbacabb.txt"));
+    let _ = std::fs::File::create(tmp_dir.path().join("some_A_filename.bin"));
+    let _ = std::fs::File::create(tmp_dir.path().join("changed_A_filename.bin"));
 
     assert_eq!(
         try_to_move(
-            Path::new("tmp/some_A_filename.bin"),
+            &tmp_dir.path().join("some_A_filename.bin"),
             Path::new("aboba/#1"),
             "(.*)",
             false
@@ -60,24 +63,31 @@ fn it_works() {
 
     assert_eq!(
         try_to_move(
-            Path::new("tmp/some_A_filename.bin"),
-            Path::new("tmp/changed_#1_filename.#2"),
+            &tmp_dir.path().join("some_A_filename.bin"),
+            &tmp_dir.path().join("changed_#1_filename.#2"),
             "some_(.*)_filename\\.(.*)",
             false
         ),
-        Err("Not able to replace existing file: tmp/changed_A_filename.bin".to_string())
+        Err("Not able to replace existing file: ".to_string()
+            + (tmp_dir
+                .path()
+                .join("changed_A_filename.bin")
+                .to_str()
+                .unwrap()))
     );
 
-    let _ = std::fs::remove_file("tmp/changed_A_filename.bin");
+    let _ = std::fs::remove_file(&tmp_dir.path().join("changed_A_filename.bin"));
     assert_eq!(
         try_to_move(
-            Path::new("tmp/some_A_filename.bin"),
-            Path::new("tmp/changed_#1_filename.#2"),
+            &tmp_dir.path().join("some_A_filename.bin"),
+            &tmp_dir.path().join("changed_#1_filename.#2"),
             "some_(.*)_filename\\.(.*)",
             false
         ),
         Ok(true)
     );
-    assert!(Path::new("tmp/changed_A_filename.bin").exists());
-    let _ = std::fs::remove_file("tmp/changed_A_filename.bin");
+    assert!(&tmp_dir.path().join("changed_A_filename.bin").exists());
+    let _ = std::fs::remove_file(tmp_dir.path().join("changed_A_filename.bin"));
+
+    let _ = tmp_dir.close();
 }
